6d155042b6f6b125187b6edf432e11aad84aee54
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2021-07-07 17:17:38 -0300

    no message

--- infrastructure/repository/transaction.go
+++ infrastructure/repository/transaction.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/lucasviniciosfs/codebank/domain"
 )
@@ -10,7 +11,7 @@ type TransactionRepositoryDb struct {
 	db *sql.DB
 }
 
-func NewTransactionRepositoryDB(db *sql.DB) *TransactionRepositoryDb {
+func NewTransactionRepositoryDb(db *sql.DB) *TransactionRepositoryDb {
 	return &TransactionRepositoryDb{db: db}
 }
 
@@ -55,11 +56,11 @@ func (t *TransactionRepositoryDb) updateBalance(creditCard domain.CreditCard) er
 }
 
 func (t *TransactionRepositoryDb) CreateCreditCard(creditCard domain.CreditCard) error {
-	stmt, err := t.db.Prepare(`insert into credit_cards(id, name, number, expiration_month,expiration_year, CVV,balance, balance_limit) 
-								values($1,$2,$3,$4,$5,$6,$7,$8)`)
-	if err != nil {
-		return err
-	}
+	stmt, err := t.db.Prepare(`insert into credit_cards(id, name, number, expiration_month, expiration_year, CVV, balance, balance_limit) values($1,$2,$3,$4,$5,$6,$7,$8)`)
+
+	// if err != nil {
+	// 	return err
+	// }
 	_, err = stmt.Exec(
 		creditCard.ID,
 		creditCard.Name,
@@ -78,4 +79,16 @@ func (t *TransactionRepositoryDb) CreateCreditCard(creditCard domain.CreditCard)
 		return err
 	}
 	return nil
+}
+
+func (t *TransactionRepositoryDb) GetCreditCard(creditCard domain.CreditCard) (domain.CreditCard, error) {
+	var c domain.CreditCard
+	stmt, err := t.db.Prepare("select id, balance, balance_limit from credit_cards where number=$1")
+	if err != nil {
+		return c, err
+	}
+	if err = stmt.QueryRow(creditCard.Number).Scan(&c.ID, &c.Balance, &c.Limit); err != nil {
+		return c, errors.New("credit card does not exists")
+	}
+	return c, nil
 }
